e668f7a0e6da584eff577c037e945d89a9e405e9
Mustafa AL Majed <mustafa.almajed1997@gmail.com>
2020-12-30 11:52:31 +0300

    Finalizing Work

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,8 +7,11 @@
     <div class='jumbotron text-center'>
         <h1> {{$title}}</h1>
         <p> Testing larvel</p>
-        <p><a class='btn btn-primary btn-lg' href='/login' role='button'>Login</a>
+        @if(Auth::guest())
+        <p>
+            <a class='btn btn-primary btn-lg' href='/login' role='button'>Login</a>
             <a class='btn btn-success btn-lg' href='/register' role='button'>Register</a>
         </p>
+        @endif
     </div>
 @endsection
